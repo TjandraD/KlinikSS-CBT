5aa4cfecf22cf1a253b68157781b78e392cf1a84
TjandraD <t.darmo.t@gmail.com>
2020-01-10 07:39:57 +0700

    Create new login authentication

--- app/src/main/java/com/tdarmo/klinikss/activity/LoginActivity.kt
+++ app/src/main/java/com/tdarmo/klinikss/activity/LoginActivity.kt
@@ -6,10 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.tdarmo.klinikss.R
+import com.tdarmo.klinikss.prevalent.Prevalent
+import io.paperdb.Paper
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val refDoctor = FirebaseDatabase.getInstance().getReference("Doctor")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +43,6 @@ class LoginActivity : AppCompatActivity() {
                                 val intent = Intent(this, AdminDashboard::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
                             }
-                            FirebaseAuth.getInstance().currentUser?.email?.takeLast(11) == "@doctor.com" -> {
-                                val intent = Intent(this, DoctorDashboard::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                                startActivity(intent)
-                            }
                             else -> {
                                 val intent = Intent(this, Dashboard::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                                 startActivity(intent)
@@ -47,8 +51,30 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
                 .addOnFailureListener{
-                    Log.d("Main", "Gagal masuk: ${it.message}")
-                    Toast.makeText(this, "E-mail/Password salah", Toast.LENGTH_SHORT).show()
+                    refDoctor.addListenerForSingleValueEvent(object: ValueEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+
+                        }
+
+                        override fun onDataChange(p0: DataSnapshot) {
+                            if(p0.child(email).exists() and (p0.child(email).child("Password").value == password)){
+                                val name = p0.child(email).child("Name").value.toString()
+                                Paper.book().write(Prevalent.doctorName, name)
+
+                                val intent = Intent(this@LoginActivity, DoctorDashboard::class.java).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                startActivity(intent)
+                            }
+                            else if(p0.child(email).exists() and (p0.child(email).child("Password").value != password)){
+                                Toast.makeText(this@LoginActivity, "Password Anda salah, mohon coba lagi", Toast.LENGTH_SHORT).show()
+                            }
+                            else{
+                                Toast.makeText(this@LoginActivity, "Account with this email $email doesn't exist, please register first", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+
+//                    Log.d("Main", "Gagal masuk: ${it.message}")
+//                    Toast.makeText(this, "E-mail/Password salah", Toast.LENGTH_SHORT).show()
                 }
         }
 
